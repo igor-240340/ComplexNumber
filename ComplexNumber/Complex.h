65646ba7a35a08373b11b526094c5efe706c3b12
igor <igor.240340@gmail.com>
2025-07-28 16:40:31 +0700

    Implement complex numbers
    Completed.

--- ComplexNumber/Complex.h
+++ ComplexNumber/Complex.h
@@ -22,6 +22,27 @@ public:
         return Complex{ new_re, new_im };
     };
 
+    // NOTE: Определение деления комплексных чисел.
+    // b * x = a | * b', где b' - сопряжённое к b.
+    // b * b' * x = a * b'.
+    // |b|^2 * x = a * b'.
+    // x = a * b' * (1 / |b|^2).
+    Complex operator/(const Complex& b) const {
+        Complex b_conj = b.get_conjugate();
+        float b_modulus = b.get_modulus();
+        Complex b_modulus_squared_inv = Complex{ 1.0f / (b_modulus * b_modulus), 0.0f };
+
+        return *this * b_conj * b_modulus_squared_inv;
+    };
+
+    Complex get_conjugate() const {
+        return Complex{ re, -im };
+    }
+
+    float get_modulus() const {
+        return std::sqrt(re * re + im * im);
+    }
+
     std::string to_string() const {
         std::string a_str = std::to_string(re);
         std::string b_abs_str = std::to_string(std::abs(im));
--- ComplexNumber/main.cpp
+++ ComplexNumber/main.cpp
@@ -7,10 +7,12 @@ void test_complex_constructor();
 void test_complex_add();
 void test_complex_sub();
 void test_complex_mul();
+void test_complex_div();
+void test_complex_get_conjugate();
+void test_complex_get_modulus();
 
 int main() {
     test_complex();
-
     return 0;
 }
 
@@ -19,33 +21,75 @@ void test_complex() {
     test_complex_add();
     test_complex_sub();
     test_complex_mul();
+    test_complex_div();
+    test_complex_get_conjugate();
+    test_complex_get_modulus();
 }
 
 void test_complex_constructor() {
+    std::cout << "TEST CONSTRUCTOR:\n";
+
     Complex a{ 13.0f, -5.0f };
-    std::cout << a.to_string() << '\n';
+    std::cout << a.to_string() << "\n\n";
 }
 
 void test_complex_add() {
+    std::cout << "TEST ADD:\n";
+
     Complex a{ 13.0f, -5.0f };
     Complex b{ 4.0f, 7.0f };
     Complex c = a + b;
 
-    std::cout << std::format("({}) + ({}) = {}", a.to_string(), b.to_string(), c.to_string()) << '\n';
+    std::cout << std::format("({}) + ({}) = {}", a.to_string(), b.to_string(), c.to_string()) << "\n\n";
 }
 
 void test_complex_sub() {
+    std::cout << "TEST SUB:\n";
+
     Complex a{ 13.0f, -5.0f };
     Complex b{ 4.0f, 7.0f };
     Complex c = a - b;
 
-    std::cout << std::format("({}) - ({}) = {}", a.to_string(), b.to_string(), c.to_string()) << '\n';
+    std::cout << std::format("({}) - ({}) = {}", a.to_string(), b.to_string(), c.to_string()) << "\n\n";
 }
 
 void test_complex_mul() {
+    std::cout << "TEST MUL:\n";
+
     Complex a{ 13.0f, -5.0f };
     Complex b{ 4.0f, 7.0f };
     Complex c = a * b;
-    
-    std::cout << std::format("({}) * ({}) = {}", a.to_string(), b.to_string(), c.to_string()) << '\n';
+
+    std::cout << std::format("({}) * ({}) = {}", a.to_string(), b.to_string(), c.to_string()) << "\n\n";
+}
+
+void test_complex_div() {
+    std::cout << "TEST DIV:\n";
+
+    Complex a{ 13.0f, -5.0f };
+    Complex b{ 4.0f, 7.0f };
+    Complex c = a / b;
+
+    std::cout << std::format("({}) / ({}) = {}", a.to_string(), b.to_string(), c.to_string()) << '\n';
+    std::cout << std::format("({}) * ({}) = {}", c.to_string(), b.to_string(), (c * b).to_string()) << "\n\n";
+}
+
+void test_complex_get_conjugate() {
+    std::cout << "TEST CONJUGATE:\n";
+
+    Complex a{ 13.0f, -5.0f };
+    Complex a_conjugate = a.get_conjugate();
+
+    std::cout << std::format("a = {}", a.to_string()) << '\n';
+    std::cout << std::format("a* = {}", a_conjugate.to_string()) << "\n\n";
+}
+
+void test_complex_get_modulus() {
+    std::cout << "TEST MODULUS:\n";
+
+    Complex a{ 13.0f, -5.0f };
+    float a_modulus = a.get_modulus();
+
+    std::cout << std::format("a = {}", a.to_string()) << '\n';
+    std::cout << std::format("|a| = {}", a_modulus) << "\n\n";
 }
